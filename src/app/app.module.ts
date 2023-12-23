cb6712cff7841d54d73f3723e829271beb72d9e3
Arthur Van Loo <arthur@vanloo.biz>
2023-12-23 19:13:53 +0100

    update 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CVComponent } from './Components/cv/cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CVComponent
   ],
   imports: [
     BrowserModule,
